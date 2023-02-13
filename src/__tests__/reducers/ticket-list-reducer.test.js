934d25df0c702098e9ac1cd4997c90671072bd74
Alex Johnson <alex.johnson293@gmail.com>
2023-02-13 10:17:49 -0800

    good fail on delete test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -3,13 +3,43 @@ import ticketListReducer from '../../reducers/ticket-list-reducer';
 describe ('ticketListReducer', () => {
   //declare constants for all tests
   let action;
+  const currentState = {
+    1: {
+    names: 'Garak and Bashir',
+    location: 'Quarks',
+    issue: 'Redux action is not working.',
+    id: 1
+  }, 2: {
+    names: 'Sisko and Jadzia',
+    location: 'Bajor',
+    issue: 'The Dominion is messing everything up again.',
+    id: 2    
+    }
+  }
+
   const ticketData = {
     names: 'Garak and Bashir',
     location: 'Quarks',
     issue: 'Redux action is not working.',
     id: 1
   };
-  
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {
+        names: 'Sisko and Jadzia',
+        location: 'Bajor',
+        issue: 'The Dominion is messing everything up again.',
+        id: 2  
+      }
+    });
+  });
+
+
   test('Should successfully add new ticket data to mainTicketList', () => {
     const {names, location, issue, id} = ticketData;
     action = {
@@ -27,7 +57,7 @@ describe ('ticketListReducer', () => {
         issue: issue,
         id: id
       }
-    })
+    });
   });
 
   test('Should return default state if no action type is passed into the reducer', ()=> {
