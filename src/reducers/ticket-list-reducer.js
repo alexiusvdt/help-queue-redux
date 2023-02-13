d4cc30f1b0c2d4070560d1c23541441eea779559
Alex Johnson <alex.johnson293@gmail.com>
2023-02-13 10:11:16 -0800

    passes second test

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,5 +1,22 @@
 const reducer = (state = {}, action) => {
-  return state;
+  //ES6 object destructuring to take properties from action object into vars
+  const { names, location, issue, id } = action;
+  switch (action.type) {
+    case 'ADD_TICKET':
+      // object.assign clones the state obj, requiring 3 args: empty obj {} (to prevent mutating state of original),
+      // object to be cloned, here ticketlist state, and returning value from object.assign(). 
+      // this basically makes a copy of the state we passed in, changes the copy, then returns the altered version.
+      return Object.assign({}, state, {
+        [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+    default:
+      return state;  
+  }
 };
 
 export default reducer;
