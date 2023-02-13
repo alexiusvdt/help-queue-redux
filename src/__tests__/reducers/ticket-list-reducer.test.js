899318807b9bb65a5aa94141ff36c0cd6a628a87
Alex Johnson <alex.johnson293@gmail.com>
2023-02-13 09:51:37 -0800

    good fail on first test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,7 +1,7 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe ('ticketListReducer', () => {
-  TextDecoderStream('Should return default state if no action type is passed into the reducer', ()=> {
+  test('Should return default state if no action type is passed into the reducer', ()=> {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 });
