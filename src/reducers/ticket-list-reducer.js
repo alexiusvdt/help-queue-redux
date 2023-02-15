f9e67b1596ea6d0c4c7c67c534686eda0f65ae86
Alex Johnson <alex.johnson293@gmail.com>
2023-02-15 14:29:24 -0800

    refactor reducers to use action constants

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,8 +1,11 @@
+import * as c from './../actions/ActionTypes';
+
+
 const reducer = (state = {}, action) => {
   //ES6 object destructuring to take properties from action object into vars
   const { names, location, issue, id } = action;
   switch (action.type) {
-    case 'ADD_TICKET':
+    case c.ADD_TICKET:
       // object.assign clones the state obj, requiring 3 args: empty obj {} (to prevent mutating state of original),
       // object to be cloned, here ticketlist state, and returning value from object.assign(). 
       // this basically makes a copy of the state we passed in, changes the copy, then returns the altered version.
@@ -14,7 +17,7 @@ const reducer = (state = {}, action) => {
         id: id
       }
     });    
-    case 'DELETE_TICKET':
+    case c.DELETE_TICKET:
       //create shallow copy, delete index of [id] and return new obj...clean!
       let newState = { ...state };
       delete newState[id];
