ef0b4d704d1761678f28d7157ed7a3ea10f72ec9
Alex Johnson <alex.johnson293@gmail.com>
2023-02-15 10:33:51 -0800

    pass rootreducer tests

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -4,6 +4,7 @@ import { legacy_createStore as createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
+//we need this for our sanity checks
 let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
@@ -16,6 +17,7 @@ describe("rootReducer", () => {
     });
   });
 
+  // 'sanity check' tests: did we combine our stuff correctly?
   test('Check that initial state of ticketListReducer matches root reducer', () => {
     expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, { type: null }));
   });
@@ -23,4 +25,27 @@ describe("rootReducer", () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
+  
+  //set the action & dispatch to store
+  //root reducer _should_ pass these correctly & be equal to the appropriate return
+  test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_TICKET',
+      names: 'Sisko and Jadzia',
+      location: 'Bajor',
+      issue: 'The Dominion is messing everything up again.',
+      id: 1  
+    }
+    store.dispatch(action);
+    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
+  });
+  
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
